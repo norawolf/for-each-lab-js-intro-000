c8e68ee2a1899667e72631547852c574d3ce2c22
Nora Vogt <noraevogt@gmail.com>
2018-10-30 21:40:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  let array = ['dog', 'cat', 'horse'];
+  array.forEach(callback);
+  return array; 
+}
