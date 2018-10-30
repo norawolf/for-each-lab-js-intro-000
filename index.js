4bc64c719f5ce21255cc48eba3b55b00ea822712
Nora Vogt <noraevogt@gmail.com>
2018-10-30 21:55:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  let array = ['pizza', 'velvet', 'blood'];
+  array.forEach(callback);
+  return(array);
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
